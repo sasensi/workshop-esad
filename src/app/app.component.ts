f705844bb8a8b0588d214d4f7a073d63f94d85e1
sasensi <asensi.samuel@gmail.com>
2021-12-13 17:25:14 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,14 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class AppComponent
 {
     data = [
-        { url: 'https://csb-xtjf0-ieo9rpqb1-sasensi.vercel.app/', name: '' },
-        { url: 'https://csb-xtjf0-ieo9rpqb1-sasensi.vercel.app/', name: '' },
-        { url: 'https://csb-xtjf0-ieo9rpqb1-sasensi.vercel.app/', name: '' },
+        { url: 'https://csb-olttg-2lfpb81jc-sasensi.vercel.app/', name: 'thomas' },
+        { url: 'https://csb-m9ju5-3w6ti6k9f-sasensi.vercel.app/', name: 'isaure' },
+        { url: 'https://csb-z6z8r-o5ad88ye8-sasensi.vercel.app/', name: 'david' },
+        { url: 'https://csb-vetk5-jb4uc8xw9-sasensi.vercel.app/', name: 'morganne' },
+        { url: 'https://csb-7pmhz-ccobadmiz-sasensi.vercel.app/', name: 'marjorie' },
+        { url: 'https://csb-10rxj-1u975er62-sasensi.vercel.app/', name: 'heloïse' },
+        { url: 'https://csb-3g282-bqpfwqht0-sasensi.vercel.app/', name: 'kassandra' },
+        { url: 'https://csb-d8h45-bn5r22pms-sasensi.vercel.app/', name: 'maggie' },
     ]
         .map(( { url, name } ) => ({ url: this.domSanitizer.bypassSecurityTrustResourceUrl(url), name }))
         .sort(() => Math.random() > 0.5 ? 1 : -1);
